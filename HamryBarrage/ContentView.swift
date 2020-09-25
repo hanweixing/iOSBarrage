d4269ed6101269742f6e6c20b3e15364ecb58d16
hanweixing <2651389599@qq.com>
2020-09-25 15:23:04 +0800

    Initial Commit

---
+++ HamryBarrage/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  HamryBarrage
+//
+//  Created by 韩卫星 on 2020/9/25.
+//  Copyright © 2020 Hamry. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
